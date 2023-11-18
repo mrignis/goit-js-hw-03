1d34e977aa0c09f67f4b8b20eb1ccfb6d6a2ca6a
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2023-11-18 11:01:43 -0500

    гз

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,11 @@
-function filterArray(numbers, value) { 
-    let  filteredArray = []; 
-    for (i = 0; i < numbers.length; i++) {
-      if (numbers[i] > value) {
-        filteredArray.push(numbers[i]);
-      }
+function filterArray(numbers, value) {
+  let filteredArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredArray.push(numbers[i]);
     }
+  }
+  return filteredArray; 
 }
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
